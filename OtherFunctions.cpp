688b47205a4ffd15aeaded1b1ad0a761f06db982
t-aubier <thomas.aubier@yahoo.fr>
2022-06-20 12:56:20 -0400

    update/comment other functions

--- OtherFunctions.cpp
+++ OtherFunctions.cpp
@@ -11,19 +11,29 @@
 #include <cstdlib>
 
 #include <iterator>
-#include <boost/random/uniform_01.hpp>
-#include <boost/random/mersenne_twister.hpp>
 
-std::random_device rd;
-boost::random::mt19937 gen(rd());
 
+// #### UNCOMMENT TO USE THE BOOST PACKAGE; comments also the lines annotated 'boost' below
+// #include <boost/random/uniform_01.hpp>
+// #include <boost/random/mersenne_twister.hpp>
+// #include <boost/random/discrete_distribution.hpp>
+// std::random_device rd;
+// boost::random::mt19937 gen(rd());
+
+
+// #### UNCOMMENT NOT TO USE THE BOOST PACKAGE
+//following lines were commented and edited:
+//std::random_device rd;
+//std::mt19937 gen(rd());
+//https://stackoverflow.com/questions/65788866/stdrandom-on-windows-always-i-get-the-same-numbers
+std::mt19937 gen(static_cast<unsigned int>(time(nullptr)));
 
 
 
 //##### INITIALIZE VECTORS
 
 std::vector<int> uniformVectorInt(const int& sizeVec, const int& value){
-    std::vector<int> vec; 
+    std::vector<int> vec;
     vec.reserve(sizeVec);
     for (int i = 0; i < sizeVec; ++i) {
         vec.push_back(value);
@@ -31,7 +41,7 @@ std::vector<int> uniformVectorInt(const int& sizeVec, const int& value){
     return vec;
 }
 std::vector<double> uniformVectorDouble(const int& sizeVec, const double& value){
-    std::vector<double> vec; 
+    std::vector<double> vec;
     vec.reserve(sizeVec);
     for (int i = 0; i < sizeVec; ++i) {
         vec.push_back(value);
@@ -85,14 +95,14 @@ void print(size_t nb){
 }
 
 void print(std::string str, std::vector<int> vec){
-    std::cout << str << " : ";        
+    std::cout << str << " : ";
     for (auto iter : vec) {
         std::cout << iter << " ";
     }
     std::cout << std::endl;
 }
 void print(std::string str, std::vector<double> vec){
-    std::cout << str << " : ";        
+    std::cout << str << " : ";
     for (auto iter : vec) {
         std::cout << iter << " ";
     }
@@ -140,14 +150,52 @@ double mean(const std::vector<double>& vec){
 double mean(const std::vector<int>& vec){
     return std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
 }
-double sd(const std::vector<int>& vec){
-    return std::sqrt(std::inner_product(vec.begin(), vec.end(), vec.begin(), 0.0) / vec.size() - mean(vec) * mean(vec));
-}
 double sd(const std::vector<double>& vec){
     return std::sqrt(std::inner_product(vec.begin(), vec.end(), vec.begin(), 0.0) / vec.size() - mean(vec) * mean(vec));
 }
+double sd(const std::vector<int>& vec){
+    return std::sqrt(std::inner_product(vec.begin(), vec.end(), vec.begin(), 0.0) / vec.size() - mean(vec) * mean(vec));
+}
+double sgn(const double& val){
+    if(val>0.0){
+        return 1.0;
+    }else if(val<0.0){
+        return -1.0;
+    }else{
+        return 0.0;
+    }
+}
+template<typename T> static inline double Lerp(T v0, T v1, T t){
+    return (1 - t)*v0 + t*v1;
+}
+template<typename T> static inline std::vector<T> Quantile(const std::vector<T>& inData, const std::vector<T>& probs){
+    if (inData.empty()){
+        return std::vector<T>();
+    }
 
+    if (1 == inData.size()){
+        return std::vector<T>(1, inData[0]);
+    }
 
+    std::vector<T> data = inData;
+    std::sort(data.begin(), data.end());
+    std::vector<T> quantiles;
+
+    for (size_t i = 0; i < probs.size(); ++i){
+        T poi = Lerp<T>(-0.5, data.size() - 0.5, probs[i]);
+
+        size_t left = std::max(int64_t(std::floor(poi)), int64_t(0));
+        size_t right = std::min(int64_t(std::ceil(poi)), int64_t(data.size() - 1));
+
+        T datLeft = data.at(left);
+        T datRight = data.at(right);
+
+        T quantile = Lerp<T>(datLeft, datRight, poi - left);
+
+        quantiles.push_back(quantile);
+    }
+    return quantiles;
+}
 
 //##### RANDOM SAMPLING
 
@@ -156,26 +204,51 @@ double randomDouble(){
     std::uniform_real_distribution<> dis(0, 1);
     return dis(gen);
 }
+double randomDouble(double minValue, double maxValue){
+    std::uniform_real_distribution<> dis(minValue, maxValue);
+    return dis(gen);
+}
+double randomDoubleExpDist(double meanValue){
+    std::exponential_distribution<> expdist(1/meanValue);
+    return expdist(gen);
+}
 int nrand(int min, int max){   // [min, max]
     std::uniform_int_distribution<int> uni(min,max);
     return uni(gen);
 }
 
-std::vector<int> sampleFromDiscreteDistributionWithReplacement(const int& sampleSize, std::vector<double> weights){
-        //New
-//     // sum all weights    
+std::vector<int> sampleFromUniformDiscreteDistributionWithReplacement(const int& sampleSize, const int& maxInt){
+    std::vector<int> samples;
+    samples.reserve(sampleSize);
+    std::uniform_int_distribution<> distribution(0, maxInt);
+    for (auto iter = 0; iter < sampleSize; iter++) {
+        samples.push_back(distribution(gen));
+    }
+    return samples;
+}
+
+
+std::vector<int> sampleFromDiscreteDistributionWithReplacement(const int& sampleSize, std::vector<double>& weights){
+
+    // Here are old versions of this function; I keep them here just in case there is a way to optimize them
+    // Isaac, you can give a try. This is a function that is very time-consuming; so if we manage to save time on this
+    // one that would be great!
+
+        // Version 1
+
+//     // sum all weights
 //     double sum_of_weight = 0;
 //     for(size_t i=0; i<weights.size(); i++) {
 //         sum_of_weight += weights[i];
 //     }
-//     
+//
 //     // sample random numbers
-//     std::vector<double> samplesRandWeight; 
+//     std::vector<double> samplesRandWeight;
 //     for (int sampleIndex = 0; sampleIndex < sampleSize; sampleIndex++) {
 //         samplesRandWeight.push_back(randomDouble()*sum_of_weight);
 //     }
-//     
-//     std::vector<int> samples = uniformVectorInt(sampleSize, -1); 
+//
+//     std::vector<int> samples = uniformVectorInt(sampleSize, -1);
 //     int nbFound = 0;
 //     int indexWeight = 0;
 //     while (nbFound<sampleSize && indexWeight<weights.size()){
@@ -190,21 +263,22 @@ std::vector<int> sampleFromDiscreteDistributionWithReplacement(const int& sample
 //         }
 //         ++indexWeight;
 //     }
-    
-    //New
-//     // sum all weights    
+
+        // Version 2
+
+//     // sum all weights
 //     double sum_of_weight = 0;
 //     for(size_t i=0; i<weights.size(); i++) {
 //         sum_of_weight += weights[i];
 //     }
-//     
+//
 //     // sample random numbers
-//     std::vector<double> samplesRandWeight; 
+//     std::vector<double> samplesRandWeight;
 //     for (int sampleIndex = 0; sampleIndex < sampleSize; sampleIndex++) {
 //         samplesRandWeight.push_back(randomDouble()*sum_of_weight);
 //     }
-//     
-//     std::vector<int> samples = uniformVectorInt(sampleSize, -1); 
+//
+//     std::vector<int> samples = uniformVectorInt(sampleSize, -1);
 //     for(int i=0; i<weights.size(); i++) {
 //         for (size_t j=0; j<samplesRandWeight.size(); j++) {
 //             if (samples[j]==-1) {
@@ -214,61 +288,92 @@ std::vector<int> sampleFromDiscreteDistributionWithReplacement(const int& sample
 //             }
 //         }
 //     }
-//     
-    //Old
-    // total size vector taken from weights vector
-    std::vector<int> samples; 
+//
+        // Version 3
+//     std::vector<int> samples;
+//     samples.reserve(sampleSize);
+//     std::discrete_distribution<int> distribution(weights.begin(), weights.end());
+//     for (auto iter = 0; iter < sampleSize; iter++) {
+//         samples.push_back(distribution(gen));
+//     }
+
+
+        // Version 4
+    std::vector<int> samples;
     samples.reserve(sampleSize);
-    std::discrete_distribution<int> distribution(weights.begin(), weights.end()); 
+//     boost::random::discrete_distribution<int> distribution(weights.begin(), weights.end()); // with the boost package
+    std::discrete_distribution<int> distribution(weights.begin(), weights.end()); // without the boost package
     for (auto iter = 0; iter < sampleSize; iter++) {
         samples.push_back(distribution(gen));
     }
 
-        
+
     return samples;
 }
 
-// std::vector<int> sampleFromDiscreteDistribution(const int& sampleSize, std::vector<double> weights){
-//     // total size vector taken from weights vector
-//     const size_t totalSize = weights.size();
-//     std::vector<int> samples; 
-//     
-//     if ((int)totalSize <= sampleSize) {   // sample size is high; all numbers are picked
-//         
-//         samples.reserve(totalSize);
-//         for (int i=0; i<totalSize; ++i) {
-//             samples.push_back(i);
-//         }
-//         
-//     }else{ // sample size is small; a subset is sample without replacement
-// 
-//         // from :
-//         // https://stackoverflow.com/questions/53632441/c-sampling-from-discrete-distribution-without-replacement
-//         // see also
-//         // https://stackoverflow.com/questions/48591592/how-do-i-use-boost-random
-//         
-//         // sudo apt-get install libboost-all-dev
-//         // g++ -std=c++14 -w testOtherFunctions.cpp OtherFunctions.cpp 
-// 
-//         boost::random::uniform_01<> dist;
-//         std::vector<double> vals;
-//         for (auto iter : weights) {
-//             vals.push_back(std::pow(dist(gen), 1. / iter));
-//         }
-//         // Sorting vals, but retain the indices. 
-//         // There is unfortunately no easy way to do this with STL.
-//         std::vector<std::pair<int, double>> valsWithIndices;
-//         for (size_t iter = 0; iter < vals.size(); iter++) {
-//             valsWithIndices.emplace_back(iter, vals[iter]);
-//         }
-//         std::sort(valsWithIndices.begin(), valsWithIndices.end(), [](auto x, auto y) {return x.second > y.second; });
-// 
-//         for (auto iter = 0; iter < sampleSize; iter++) {
-//             samples.push_back(valsWithIndices[iter].first);
-//         }     
-//     }
-//     return samples;
-// }
+ // does not work but could be useful for later
+std::vector<int> sampleFromDiscreteDistribution(const int& sampleSize, std::vector<double> weights){
+    // total size vector taken from weights vector
+    const size_t totalSize = weights.size();
+    std::vector<int> samples;
+
+    if ((int)totalSize <= sampleSize) {   // sample size is high; all numbers are picked
+
+        samples.reserve(totalSize);
+        for (int i=0; i<totalSize; ++i) {
+            samples.push_back(i);
+        }
+
+    }else{ // sample size is small; a subset is sample without replacement
+
+        samples.reserve(sampleSize);
+
+        // from :
+        // https://stackoverflow.com/questions/53632441/c-sampling-from-discrete-distribution-without-replacement
+        // see also
+        // https://stackoverflow.com/questions/48591592/how-do-i-use-boost-random
+
+        // sudo apt-get install libboost-all-dev
+        // g++ -std=c++14 -w testOtherFunctions.cpp OtherFunctions.cpp
+
+       // boost::random::uniform_01<> dist;
+        std::uniform_real_distribution<> dist(0,1);
+        std::vector<double> vals;
+        for (auto iter : weights) {
+            vals.push_back(std::pow(dist(gen), 1. / iter));
+        }
+        // Sorting vals, but retain the indices.
+        // There is unfortunately no easy way to do this with STL.
+        std::vector<std::pair<int, double>> valsWithIndices;
+        for (size_t iter = 0; iter < vals.size(); iter++) {
+            valsWithIndices.emplace_back(iter, vals[iter]);
+        }
+        std::sort(valsWithIndices.begin(), valsWithIndices.end(), [](auto x, auto y) {return x.second > y.second; });
+
+        for (auto iter = 0; iter < sampleSize; iter++) {
+            samples.push_back(valsWithIndices[iter].first);
+        }
+    }
+    return samples;
+}
+
+std::vector<int> sampleFromUniformDistribution0And1(const int& sampleSize){
+    std::vector<int> samples;
+    samples.reserve(sampleSize);
+    for (int i=0; i<sampleSize; ++i) {
+        samples.push_back(nrand(0, 1));
+    }
+    return samples;
+}
+
+std::vector<double> sampleFromUniformDistributionBetween0And1(const int& sampleSize){
+    std::vector<double> samples;
+    samples.reserve(sampleSize);
+    for (int i=0; i<sampleSize; ++i) {
+        samples.push_back(randomDouble());
+    }
+    return samples;
+}
 
 std::vector<int> sampleFromNormalDiscreteDistribution(double mean, double sd, int size){
     std::normal_distribution<float> noise(mean, sd);
@@ -290,20 +395,44 @@ std::vector<double> sampleFromNormalDistribution(double mean, double sd, int siz
     return samples;
 }
 
+std::vector<double> sampleFromExponentialDistribution(double mean,int size){
+    std::exponential_distribution<float> dist(1/mean);
+    std::vector<double> samples;
+    samples.reserve(size);
+    for (int i=0; i<size; ++i) {
+        samples.push_back((double) dist(gen));
+    }
+    return samples;
+}
 
-//##### FUNCTIONS INDIVIDUALS
+std::vector<int> sampleFromBinomialDiscreteDistribution(double prob, int n, int size){
+    std::binomial_distribution<int> distribution(n,prob);
+    std::vector<int> samples;
+    samples.reserve(size);
+    for (int i=0; i<size; ++i) {
+        samples.push_back((int) round(distribution(gen)));
+    }
+    return samples;
+}
 
-// std::vector<int>
+// MAIN FILE  (COMMENTED TO RUN THE OTHER SCRIPTS)
 
-
-
-
-
-
+// #include <chrono>
+// using namespace std::chrono;
 // int main()
-// {   
-//     
-//     testRunTime();
-// 
+// {
+// //     print(randomDouble());
+// //     print(nrand(0,2));
+//
+//     std::vector<double> weights = uniformVectorDouble(500,1.0);
+//     std::vector<int> vec;
+//     auto start = high_resolution_clock::now();
+//     for(int i(0);i<10000;++i){
+//         vec=sampleFromDiscreteDistributionWithReplacement(10,weights);
+//     }
+//     auto stop = high_resolution_clock::now();
+//     auto duration = duration_cast<microseconds>(stop - start);
+//     std::cout << duration.count() << std::endl;
+//
 //     return 0;
 // }
--- OtherFunctions.h
+++ OtherFunctions.h
@@ -3,7 +3,6 @@
 
 #include <vector>
 #include <array>
-
 //####################################################################
 // Other functions
 //####################################################################
@@ -12,11 +11,31 @@
 
 //##### INITIALIZE VECTORS
 
+
+//##   Create a vector of integers
+//     @input  int sizeVec      :  the size of the vector
+//     @input  int value        :  the int value of each element of the vector
+//     @return std::vector<int> :  a uniform vector with the same int elements
 std::vector<int> uniformVectorInt(const int& sizeVec, const int& value);
+
+//##   Create a vector of double
+//     @input  int sizeVec      :  the size of the vector
+//     @input  double value     :  the double value of each element of the vector
+//     @return std::vector<int> :  a uniform vector with the same double elements
 std::vector<double> uniformVectorDouble(const int& sizeVec, const double& value);
+
+
+
+
 
 //##### PRINT TERMINAL
 
+
+//##   Print variables in the terminal
+//##   (useful for debugging)
+//     @input  the element to be printed; can be a vector, and array or a number
+//     @return print these variables
+//     note: if one wants to print another object, one can modify one of these functions
 void print(std::vector<int> vec);
 void print(std::vector<double> vec);
 void print(std::vector<std::array<double,3>> vec);
@@ -25,6 +44,12 @@ void print(double nb);
 void print(int nb);
 void print(size_t nb);
 
+//##   Print variables in the terminal with a given string before
+//##   (useful for debugging; to annotate the line to be printed)
+//     @input  the string to be printed
+//     @input  the element to be printed; can be a vector, and array or a number
+//     @return print this string and these variables
+//     note: if one wants to print another object, one can modify one of these functions
 void print(std::string str, std::vector<int> vec);
 void print(std::string str, std::vector<double> vec);
 void print(std::string str, std::vector<std::array<double,3>> vec);
@@ -34,26 +59,141 @@ void print(std::string str, int nb);
 void print(std::string str, size_t nb);
 void print(std::string str);
 
+
+
+
+
 //##### BASIC STATS
 
+
+//##   Compute the mean value of double elements stored in a vector
+//     @input  std::vector<double> vec  :  the vector containing the double elements
+//     @return double                   :  the mean values of those double elements
 double mean(const std::vector<double>& vec);
+
+//##   Compute the mean value of int elements stored in a vector
+//     @input  std::vector<double> vec  :  the vector containing the int elements
+//     @return double                   :  the mean values of those int elements
 double mean(const std::vector<int>& vec);
-double sd(const std::vector<int>& vec);
+
+//##   Compute the standard deviation of double elements stored in a vector
+//     @input  std::vector<double> vec  :  the vector containing the double elements
+//     @return double                   :  the standard deviation of those int elements
 double sd(const std::vector<double>& vec);
+
+//##   Compute the standard deviation of int elements stored in a vector
+//     @input  std::vector<double> vec  :  the vector containing the int elements
+//     @return double                   :  the standard deviation of those int elements
+double sd(const std::vector<int>& vec);
+
+//##   Compute the sign of a double value
+//     @input  double val :  the double value
+//     @return doubl      :  either -1.0 (if val<0), 0.0 (if val==0), or +1.0 (if val>0)
+double sgn(const double& val);
+
+//##   Function used in the Quantile() function (see below
+template<typename T> static inline double Lerp(T v0, T v1, T t);
+
+//##   Compute the quantile from a vector of numerics
+//     @input  std::vector<T>& inData :  the vector containing the numeric elements
+//     @input  std::vector<T>& probs  :  the quantiles
+//     @return std::vector<T>         :  the numerics corresponding to the quantiles
+template<typename T> static inline std::vector<T> Quantile(const std::vector<T>& inData, const std::vector<T>& probs);
+
+
 
 //##### RANDOM SAMPLING
 
+//##   Compute a random double number in the range [0,1]
+//     @return double                   :  the random number
 double randomDouble();
+
+//##   Compute a random double number following a uniform distribution in a given range
+//     @input  double  minValue  :  the minimum value that can be drawn
+//     @input  double  maxValue  :  the maximum value that can be drawn
+//     @return double            :  the random number belonging to the range [minValue, maxValue]
+double randomDouble(double minValue, double maxValue);
+
+//##   Compute a random double number following an exponential distribution with a given mean
+//     @input  double  meanValue :  the mean value describing the exponential distribution
+//                                  (=1/lambda with lambda being the parameter that usually describes such distribution)
+//     @return double            :  the random number drawn in this exponential distribution
+double randomDoubleExpDist(double meanValue);
+
+//##   Compute a random int number in a given range
+//     @input  int  min  :  the minimum int value that can be drawn
+//     @input  int  max  :  the maximum int value that can be drawn
+//     @return int       :  the random number belonging to the range {minValue, minValue+1, ... maxValue}
 int nrand(int min, int max);
 
-std::vector<int> sampleFromDiscreteDistributionWithReplacement(const int& sampleSize, std::vector<double> weights); // with replacement
-// std::vector<int> sampleFromDiscreteDistribution(const int& sampleSize, std::vector<double> weights); // without replacement
 
+//##   Compute a list of int number from 0 to maxInt following a uniform distribution; with replacement
+//     @input  int  sampleSize  :   the number of int numbers to sample
+//     @input  int  maxInt      :  the maximum int value that can be drawn
+std::vector<int> sampleFromUniformDiscreteDistributionWithReplacement(const int& sampleSize, const int& maxInt);
+
+//##   Compute a list of int numbers following a probability distribution *with* replacement,
+//##   so that each int number can be sampled more than once
+//     @input  int  sampleSize              :  the number of int numbers to sample
+//     @input  std::vector<double> weights  :  a vector where each double element represent the weight associated with each element
+//     @return std::vector<int>             :  a vector of size 'sampleSize' with each element drawn following its weight
+//                                             for instance, if weight = {1.0,0.1,0.1} and sampleSize = 10,
+//                                             each of the 10 elements of the output vector will be 0 with a probability  1  / 1.2
+//                                                                                                  1 with a probability 0.1 / 1.2
+//                                                                                                  2 with a probability 0.1 / 1.2
+std::vector<int> sampleFromDiscreteDistributionWithReplacement(const int& sampleSize, std::vector<double>& weights);
+
+//##   Compute a list of int numbers 0 and 1 following a uniform distribution,
+//##   so that each int number can be sampled more than once
+//     @input  int  sampleSize              :  the number of int numbers to sample
+//     @return std::vector<int>             :  a vector of size 'sampleSize' with each element being 0 or 1, following a uniform distribution
+std::vector<int> sampleFromUniformDistribution0And1(const int& sampleSize);
+
+//##   Compute a list of double numbers between 0 and 1 following a uniform distribution,
+//##   so that each int number can be sampled more than once
+//     @input  int  sampleSize              :  the number of int numbers to sample
+//     @return std::vector<double>          :  a vector of size 'sampleSize' with each element being between 0 or 1, following a uniform distribution
+std::vector<double> sampleFromUniformDistributionBetween0And1(const int& sampleSize);
+
+//##   Compute a list of int numbers following a probability distribution *without* replacement,
+//##   so that each int number can only be sampled once
+//     @input  int  sampleSize              :  the number of int numbers to sample; this number cannot be higher than the size of input weights
+//     @input  std::vector<double> weights  :  a vector where each double element represent the weight associated with each element
+//     @return std::vector<int>             :  a vector of size 'sampleSize' with each element drawn following its weight
+//                                             for instance, if weight = {1.0,0.1,0.1} and sampleSize = 3,
+//                                             the output will comprise all elements; e.g. {0,2,1}, or {2,1,0} because each element can
+//                                             only be sampled once
+std::vector<int> sampleFromDiscreteDistribution(const int& sampleSize, std::vector<double> weights);
+// does not work but could be useful for later
+
+//##   Compute a list of int numbers following a normal distribution, *with* replacement
+//     @input  double  mean                 :  the mean characterizing the normal distribution
+//     @input  double  sd                   :  the standard deviation characterizing the normal distribution
+//     @input  int  size                    :  the number of int numbers to sample
+//     @return std::vector<int>             :  a vector of size 'size' with each int element drawn following the normal distribution
 std::vector<int> sampleFromNormalDiscreteDistribution(double mean, double sd, int size);
+
+//##   Compute a list of double numbers following a normal distribution, *with* replacement
+//     @input  double  mean                 :  the mean characterizing the normal distribution
+//     @input  double  sd                   :  the standard deviation characterizing the normal distribution
+//     @input  int  size                    :  the number of int numbers to sample
+//     @return std::vector<double>          :  a vector of size 'size' with each double element drawn following the normal distribution
 std::vector<double> sampleFromNormalDistribution(double mean, double sd, int size);
 
-//##### RUNTIME FUNCTIONS
+//##   Compute a list of double numbers following an exponential distribution, *with* replacement
+//     @input  double  mean                 :  the mean characterizing the exponential distribution
+//                                             (=1/lambda with lambda being the parameter that usually describes such distribution)
+//     @input  int  size                    :  the number of int numbers to sample
+//     @return std::vector<double>             :  a vector of size 'size' with each double element drawn following the exponential distribution
+std::vector<double> sampleFromExponentialDistribution(double mean,int size);
 
-void testRunTime();
+//##   Compute a list of int numbers following a normal distribution, *with* replacement
+//     @input  double  prob                 :  parameter p describing the binomial distribution
+//     @input  int  n                       :  parameter N describing the binomial distribution
+//     @input  int  size                    :  the number of int numbers to sample
+//     @return std::vector<int>             :  a vector of size 'size' with each element drawn following the binomial distribution
+std::vector<int> sampleFromBinomialDiscreteDistribution(double prob, int n, int size);
+
+
 
 #endif
