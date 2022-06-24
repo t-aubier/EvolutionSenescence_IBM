2d0954dd6bd5f71fad14fd1ebe99a62252234e06
t-aubier <thomas.aubier@yahoo.fr>
2022-06-24 16:19:57 -0400

    comment within the functions

--- ClassIndividual.cpp
+++ ClassIndividual.cpp
@@ -29,7 +29,8 @@ Individual::Individual( const int& indexPopulation,
                         const double& convertIntoYear,
                         const std::string& typeAccumulation
                       ){
-
+    
+    // We initialize parameters and variables
     _indexPopulation = indexPopulation;
     _maxDamageConsidered = maxDamageConsidered;
     _probSurvExtrinsicMortality = probSurvExtrinsicMortality;
@@ -58,6 +59,7 @@ Individual::~Individual(){};
     ///###### CLASS METHODS
 
 void Individual::reset(){
+    // We reinititialize the individual
     _livingState = true;
     _causeDeath = -1;
     _age = 0;
@@ -65,11 +67,13 @@ void Individual::reset(){
 }
 
 void Individual::updateNewTimeStep(){
+    // At each new time step, individiduals may die of extrinsic of intrinsic mortality
+    // We then update the number of damages (i.e., the somatic state) of living individuals
     if (_livingState==true){
 
-        if (randomDouble()<_probSurvExtrinsicMortality) {
+        if (randomDouble()<_probSurvExtrinsicMortality) {   // extrinsic mortality
             if ( (_damage<_survCausedPerMutation.size()   && randomDouble()<_survCausedPerMutation[_damage])  ||
-                 (_damage>_survCausedPerMutation.size()-1 && randomDouble()<_survCausedPerMutation[_survCausedPerMutation.size()-1])    ) {
+                 (_damage>_survCausedPerMutation.size()-1 && randomDouble()<_survCausedPerMutation[_survCausedPerMutation.size()-1])    ) { // intrinsic mortality
                 ++_age;
                 getLevelDamage();
             }else{
@@ -84,6 +88,8 @@ void Individual::updateNewTimeStep(){
 }
 
 void Individual::updateNewTimeStep(double& adjustTerm){
+    // Same function as updateNewTimeStep() but this time with density-dependent extrinsic mortality
+    // Variable adjustTerm adjusts extrinsic mortality depending on population density relative to the carrying capacity
     if (_livingState==true){
         if (adjustTerm<1e-20 || randomDouble()<1+(_probSurvExtrinsicMortality-1)*adjustTerm) {
             if ( (_damage<_survCausedPerMutation.size()   && randomDouble()<_survCausedPerMutation[_damage])  ||
@@ -103,10 +109,11 @@ void Individual::updateNewTimeStep(double& adjustTerm){
 }
 
 void Individual::getLevelDamage(){
+    // We update the number of damages, i.e., the somatic state
     if(_typeAccumulation=="lin"){
-        _damage = (int) (_age * _rateAccumul);
+        _damage = (int) (_age * _rateAccumul);                      // the number of damages reflect age
     }else if(_typeAccumulation=="randomlin"){
-        double probAccum = 1-exp(-_rateAccumul/_convertIntoYear);
+        double probAccum = 1-exp(-_rateAccumul/_convertIntoYear);   // the number of damages accumulate randomly at a given rate controlled by _rateAccumul
         if (randomDouble()<probAccum) {
             _damage++;
         }
@@ -114,14 +121,18 @@ void Individual::getLevelDamage(){
 }
 
 void Individual::shortenMutationVector(bool boolReverseMutation){
+    // We shorten the vector describing intrinsic mortality depending on the number of damages
+    // in order to reduce simulation run time
     int indexEnd(-1);
     int age(0);
     while (age<_survCausedPerMutation.size() && indexEnd<0) {
+        // We shorten the vector at a given number of damages only if survival is equal to 0 and if more than 10 lethal mutations have already accumulated
         if( (boolReverseMutation==false && _survCausedPerMutation[age]<1e-20 && _nbLethalMutations[age]>0) || (boolReverseMutation==true && _survCausedPerMutation[age]<1e-20 && _nbLethalMutations[age]>10)){
             indexEnd = age;
         }
         ++age;
     }
+    // We now shorten the vector
     if(indexEnd>0 && indexEnd<_survCausedPerMutation.size()-2){
         std::vector<double> newSurvCausedPerMutation = {};
         for (int age(0); age<indexEnd+1; ++age) {
--- ClassPopulation.cpp
+++ ClassPopulation.cpp
@@ -45,14 +45,13 @@ Population::Population( const int& carryingCapaxity,
                         const double& effectSurvDeleteriousMutation
                       ){
 
+    // We initialize parameters and variables
     _carryingCapaxity = carryingCapaxity;
     _densityDependenceSurvival = densityDependenceSurvival;
     _nbOffspringPerInd = nbOffspringPerInd;
     _alphaMax = alphaMax;
     _rateAlphaFecundity = rateAlphaFecundity;
-
     _maxDamageConsidered = maxDamageConsidered;
-
     _probDeleteriousMutationPerAge = probDeleteriousMutationPerAge;
     _ratioReverseMutation = ratioReverseMutation;
     _convertIntoYear = convertIntoYear;
@@ -60,10 +59,10 @@ Population::Population( const int& carryingCapaxity,
     _boolReverseMutation = boolReverseMutation;
     _effectSurvDeleteriousMutation = effectSurvDeleteriousMutation;
 
+    // We initialize the individuals
     _ListLivingInd.reserve(_carryingCapaxity*2);
     _ListDeadInd.reserve(_carryingCapaxity*2);
     _ListAllInd.reserve(_carryingCapaxity*2);
-
     Individual ind;
     for (int indexPopulation(0); indexPopulation<_carryingCapaxity; ++indexPopulation) {
         ind = Individual( indexPopulation,
@@ -87,14 +86,14 @@ Population::~Population(){};
     ///###### CLASS METHODS
 
 void Population::updateNewTimeStep(){
-    // update living state, damage accumulation, and survival probability
-    if (_densityDependenceSurvival==true && _ListInd.size()<_carryingCapaxity){
-        // define adjustTerm that will cause density-dependent extrinsic mortality
+    // We update individuals at each time step
+    if (_densityDependenceSurvival==true && _ListInd.size()<_carryingCapaxity){             // With a density-dependent extrinsic mortality
+        // We define adjustTerm that will cause density-dependent extrinsic mortality
         double adjustTerm = ((double)_ListInd.size()) / ((double) _carryingCapaxity);
         for (auto& iter:_ListInd){
             iter.updateNewTimeStep(adjustTerm);
         }
-    }else{
+    }else{                                                                                  // Without a density-dependent extrinsic mortality
         for (auto& iter:_ListInd){
             iter.updateNewTimeStep();
         }
@@ -103,8 +102,10 @@ void Population::updateNewTimeStep(){
 
 
 void Population::replaceDeadIndividuals(){
+    // We replace dead individuals by offspring individuals at each time step
 
-    // update living/dead and list of offspring
+    // We update living/dead and list of offspring
+    // We add living individuals to the vector _ListIndUpdated
     _ListLivingInd.clear();
     _ListDeadInd.clear();
     _ListIndOfspring.clear();
@@ -115,10 +116,9 @@ void Population::replaceDeadIndividuals(){
             _ListLivingInd.push_back(iter._indexPopulation);
             _ListIndUpdated.push_back(iter);
 
-            // asexual reproduction and production of offspring
+            // We consider asexual reproduction and production of offspring
             std::poisson_distribution<int> distribution(_nbOffspringPerInd*iter._fecundity);
             int nbOffspring = distribution(gen2);
-
             for (int nbOff(0); nbOff<nbOffspring; ++nbOff) {
                 _ListIndOfspringIndex.push_back(iter._indexPopulation);
             }
@@ -128,7 +128,7 @@ void Population::replaceDeadIndividuals(){
         }
     }
 
-    // local competition among offspring
+    // We consider local competition among offspring
     double sizePop = _ListLivingInd.size() + _ListIndOfspringIndex.size();
     double probSurvOffspring = 0.0;
     if(_carryingCapaxity - (double) _ListLivingInd.size()>0){
@@ -140,14 +140,15 @@ void Population::replaceDeadIndividuals(){
         }
     }
 
+    // We add living offspring to the vector _ListIndUpdated
     for (auto& iter:_ListIndOfspring) {
-            //mutation
+            // We implement mutations
             int age(0);
             bool foundLowestAge = false;
-            while (age<iter._survCausedPerMutation.size()) {
+            while (age<iter._survCausedPerMutation.size()) {    // We loop over each damage categorie (i.e., each age categorie assuming non-stochastic linear accumulation)
 
                 if ((_boolReverseMutation==false && iter._nbLethalMutations[age]<1 &&  randomDouble()<_probDeleteriousMutationPerAge) || (_boolReverseMutation==true &&  randomDouble()<_probDeleteriousMutationPerAge)){ // HERE
-                    // deleterious mutations
+                    // Deleterious mutations
                     for(int age2(0);age2<_rangeEffectDeleteriousMutation;++age2){
                         if(age+age2<iter._survCausedPerMutation.size()){
                             iter._survCausedPerMutation[age+age2] -= _effectSurvDeleteriousMutation;
@@ -161,7 +162,7 @@ void Population::replaceDeadIndividuals(){
                     }
 
                 }else if(_boolReverseMutation==true && iter._nbLethalMutations[age]>0 && randomDouble()<_probDeleteriousMutationPerAge*_ratioReverseMutation){
-                    // beneficial 'reverse' mutations
+                    // Beneficial 'reverse' mutations
                     for(int age2(0);age2<_rangeEffectDeleteriousMutation;++age2){
                         if(age+age2<iter._survCausedPerMutation.size()-1){
                             iter._nbLethalMutations[age+age2] -= 1.0;
@@ -174,7 +175,7 @@ void Population::replaceDeadIndividuals(){
                         }
                     }
                 }
-                // change in fecundity in the case of pleiotropic mutations
+                // We consider a change in fecundity in the case of pleiotropic mutations
                 if(foundLowestAge == false && iter._survCausedPerMutation[age]<1e-30){
                     iter._fecundity = 1.0 + (_alphaMax - 1.0) * exp(-(_rateAlphaFecundity  * age));
                     foundLowestAge = true;
@@ -185,7 +186,7 @@ void Population::replaceDeadIndividuals(){
             _ListIndUpdated.push_back(iter);
     }
 
-    // update population
+    // We update the population
     _ListInd = _ListIndUpdated;
     int index2 = 0;
     for (auto& iter:_ListInd) {
@@ -195,6 +196,7 @@ void Population::replaceDeadIndividuals(){
 }
 
 void Population::shortenMutationVector(bool boolReverseMutation){
+    // We shorten the vector describing intrinsic mortality depending on the number of damages
     for (auto& iter:_ListInd)
         iter.shortenMutationVector(boolReverseMutation);
 }
@@ -203,10 +205,12 @@ void Population::shortenMutationVector(bool boolReverseMutation){
     ///###### ADDITIONAL METHODS
 
 void Population::setProbDeleteriousMutationPerAge(const double& newProbDeleteriousMutationPerAge){
+    // We change the probability of mutations
     _probDeleteriousMutationPerAge = newProbDeleteriousMutationPerAge;
 }
 
 void Population::reset(){
+    // We reset the population
     for (auto& iter:_ListInd)
         iter.reset();
 }
